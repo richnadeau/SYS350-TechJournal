d1612dac1938da61187f226bdcc16f4a1b2debd7
richnadeau <rich.nadeau@mymail.champlain.edu>
2022-02-09 22:28:14 -0500

    Milestone 4 Script Updated + Working

--- Tools/Python/menu.py
+++ Tools/Python/menu.py
@@ -1,9 +1,11 @@
 def menu():
     print("[1] VCenter Info")
-    print("[2] Option 2")
+    print("[2] Session Details")
+    print("[3] VM Details")
     print("[0] Exit the program.")
 
 import connect
+from pyVim.connect import Disconnect
 
 menu()
 option = int(input("Enter your option: "))
@@ -11,11 +13,44 @@ option = int(input("Enter your option: "))
 while option != 0:
     if option == 1:
         print("VCenter Info Option Selected.")
-        aboutInfo=si.content.about
+        aboutInfo=connect.si.content.about
         print(aboutInfo)
     elif option == 2:
-        print("Option 2 Selected.")
+        print("Session Details Selected.")
+        for session in connect.si.content.sessionManager.sessionList:
+            if session.key == connect.si.content.sessionManager.currentSession.key:
+                print("~~~~~~~~~~~~~~~~~~~~~")
+                print("username = {0.userName}".format(session))
+                print("ip = {0.ipAddress}".format(session))
+                print("vcenter host = ", connect.vcenterh)
+                print("~~~~~~~~~~~~~~~~~~~~~")
+    elif option == 3:
+        print("VM Details Selected.")
+        connect.si.RetrieveContent()
+        datacenter = connect.si.content.rootFolder.childEntity[0]
+        vms = datacenter.vmFolder.childEntity
+        vmname=input("Enter the name of your VM (leave empty if you want all printed): ")
 
+        if len(vmname) == 0:
+            for i in vms:
+                print("Name: ", i.name)
+                print("State: ", i.guest.guestState)
+                print("# of Processors: ", i.config.hardware.numCPU, " CPU(s)")
+                GB = (i.config.hardware.memoryMB/1000)
+                print("Memory Total: ", GB , " GB")
+                print("IP Address: ", i.summary.guest.ipAddress)
+                print("~~~~~~~~~~~~~~~~~~~~~") 
+        else:
+            for i in vms:               
+                if vmname in str(i.name):
+                    print("Name: ", i.name)
+                    print("State: ", i.guest.guestState)
+                    print("# of Processors: ", i.config.hardware.numCPU, " CPU(s)")
+                    GB = (i.config.hardware.memoryMB/1000)
+                    print("Memory Total: ", GB , " GB")
+                    print("IP Address: ", i.summary.guest.ipAddress)
+                    print("~~~~~~~~~~~~~~~~~~~~~")
+                        
     else:
         print("Invalid option.")
 
@@ -23,4 +58,5 @@ while option != 0:
     menu()
     option = int(input("Enter your option: "))
 
+Disconnect(connect.si)
 print("Disconnecting, Goodbye.")
