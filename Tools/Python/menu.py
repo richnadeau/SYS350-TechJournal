3220b8dab5efbebed26c584b5b06cc77dabb7908
richnadeau <rich.nadeau@mymail.champlain.edu>
2022-02-18 15:51:21 -0500

    Added Reconfiguration of VM (CPU and Memory) as an action

--- Tools/Python/menu.py
+++ Tools/Python/menu.py
@@ -11,11 +11,14 @@ def vmmenu():
     print("[2] Power Off VM")
     print("[3] Take a Snapshot")
     print("[4] Delete a VM")
+    print("[5] Reconfigure a VM")
+    print("[6] Change a VM's Network")
     print("[0] Exit the VM Actions.")
 
 from time import sleep
 import connect
 from pyVim.connect import Disconnect
+from pyVmomi import vim
 
 menu()
 option = int(input("Enter your option: "))
@@ -156,12 +159,57 @@ while option != 0:
                             print("Not deleting ", i.name)
                             print()
 
+            elif vmoption == 5:
+                print("You Have Chosen to Reconfigure a VM's Resources")
+                connect.si.RetrieveContent()
+                datacenter = connect.si.content.rootFolder.childEntity[0]
+                vms = datacenter.vmFolder.childEntity
+                confvmname=input("Enter the name of the VM you want to reconfigure: ")
+                for vm in vms:
+                    if confvmname in str(vm.name):
+                        selection=input(vm.name + " is selected, are you sure you want to reconfigure this VM? (Y/N): ")
+                        if selection == "Y":
+                            if vm.guest.guestState == "notRunning":
+                                cpuchange=input("Do you  want to change the CPU count of " + vm.name + "? Current CPU Count is " + str(vm.config.hardware.numCPU) + " (Y/N):")
+                                if cpuchange == "Y":
+                                    CPUnum=input("How many CPUs do you want for this VM: ")
+                                    Corenum=input("How many cores do you want on each CPU for this VM: ")
+                                    cpuspec = vim.vm.ConfigSpec()
+                                    cpuspec.numCPUs = int(CPUnum)
+                                    cpuspec.numCoresPerSocket = int(Corenum)
+                                    vm.Reconfigure(cpuspec)
+                                    sleep(5)
+                                    print(vm.name, "has been updated to have ", str(vm.config.hardware.numCPU), " CPUs.")
+                                    print()
+                                elif cpuchange == "N":
+                                    print("No changes will be done to the CPU count of ", vm.name)
+                                    print()
+                                memchange=input("Do you  want to change the amount of memory on " + vm.name + "? Current amount of memory is " + str(vm.config.hardware.memoryMB/1024) + " GB (Y/N):")
+                                if memchange == "Y":
+                                    memnum=input("How much memory do you want for this VM (in GB): ")
+                                    memnum = int(memnum)
+                                    memnum = memnum*1024
+                                    memspec = vim.vm.ConfigSpec()
+                                    memspec.memoryMB = memnum
+                                    vm.Reconfigure(memspec)      
+                                    sleep(5)
+                                    print(vm.name, "has been updated to have ", str(vm.config.hardware.memoryMB/1024), " GB of memory.")
+                                    print()                              
+                                elif memchange == "N":
+                                    print("No changes will be done to the memory amount of ", vm.name)
+                            else:
+                                print(vm.name, " is currently powered on! You will need to power it down before reconfiguring.")
+                                print()
+                        elif selection == "N":
+                            print()       
+            elif vmoption == 6:
+                print("W.I.P")
+
             else:
-                print ("Invalid option.")
+                print("Invalid option.")
             print()
             vmmenu()
             vmoption = int(input("Enter your option: "))
-
                         
     else:
         print("Invalid option.")
